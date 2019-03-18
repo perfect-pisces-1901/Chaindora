6a38042125f7972d32fa05772ae47a1ae5f7a550
alvinjtang <alvintangdesigns@gmail.com>
2019-03-17 20:46:03 -0400

    HashRouter to BrowserRouter

--- client/Routes.js
+++ client/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, HashRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import AllSongs from "./components/AllSongs";
 import Login from "./components/Login";
 import ArtistUpload from "./components/ArtistUpload";
@@ -8,14 +8,14 @@ import HomePage from "./components/HomePage";
 export default class Routes extends Component {
   render() {
     return (
-      <HashRouter>
+      <Switch>
         <div id="main">
           <Route exact path="/" component={HomePage} />
           <Route path="/SongsList" component={AllSongs} />
           <Route path="/Login" component={Login} />
           <Route path="/Artist" component={ArtistUpload} />
         </div>
-      </HashRouter>
+      </Switch>
     );
   }
 }
