2f43c5bee6dcb842730f95d4284d6dd659edb72a
Ines <Ines@MacBook-Pro-de-Ines-2.local>
2019-03-19 18:15:05 -0400

    feature: Add Payment function on smart contract

--- contracts/storage_contract.sol
+++ contracts/storage_contract.sol
@@ -2,6 +2,7 @@ pragma solidity >= 0.4.17;
 
 contract Contract {
   string ipfsHash;
+  uint amount = 2 ether;
 
   function setHash(string memory x) public {
     ipfsHash = x;
@@ -10,4 +11,10 @@ contract Contract {
   function getHash() public view returns (string memory x) {
     return ipfsHash;
   }
+
+  function payArtist(address payable artist ) payable public {
+      artist.transfer(amount);
+  }
+ 
+
 }
--- src/storehash.js
+++ src/storehash.js
@@ -1,6 +1,5 @@
 /* TODO -- this is copied from the truffle compiler output - automate... */
 
-
 /* eslint-disable quotes */
 /* eslint-disable quote-props */
 import web3 from "./web3";
@@ -8,34 +7,46 @@ import web3 from "./web3";
 const address = "0x132ea5526845E5A4bb8E5Bd0707f09eC7299d8E8";
 const abi = [
   {
-    "constant": false,
-    "inputs": [
+    constant: false,
+    inputs: [
       {
-        "name": "x",
-        "type": "string"
+        name: "x",
+        type: "string"
       }
     ],
-    "name": "setHash",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function",
-    "signature": "0x1ed83fd4"
+    name: "setHash",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
   },
   {
-    "constant": true,
-    "inputs": [],
-    "name": "getHash",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: "getHash",
+    outputs: [
       {
-        "name": "x",
-        "type": "string"
+        name: "x",
+        type: "string"
       }
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function",
-    "signature": "0xd13319c4"
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "artist",
+        type: "address"
+      }
+    ],
+    name: "payArtist",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
   }
 ];
 export default new web3.eth.Contract(abi, address);
