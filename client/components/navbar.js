a5ae78d49c6fff5445161f7189906917d1705fe1
Ines <Ines@MacBook-Pro-de-Ines-2.local>
2019-03-20 00:47:15 -0400

    feature: design HomePage and AllSongs Component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Navbar = props => {
-  return (
-    <div id="navbar" className="row">
-      <h3>CHAINDORA</h3>
-    </div>
-  );
+  return <div id="navbar" className="row" />;
 };
 
 export default Navbar;
