60cb2923a4a9a127e3ae29205068f0ffc3d2b781
alvinjtang <alvintangdesigns@gmail.com>
2019-03-20 11:05:48 -0400

    updates navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import {NavLink} from "react-router-dom"
 
 const Navbar = props => {
-<<<<<<< HEAD
   return (
     <div id="navbar" className="row">
       <NavLink className="link" to="/">
@@ -13,9 +12,6 @@ const Navbar = props => {
       <NavLink className="link" to="/SongsList">Listen</NavLink>
     </div>
   );
-=======
-  return <div id="navbar" className="row" />;
->>>>>>> a5ae78d49c6fff5445161f7189906917d1705fe1
 };
 
 export default Navbar;
