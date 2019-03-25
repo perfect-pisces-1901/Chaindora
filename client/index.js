d4ba88e21ed8033ebe94d40233079870ace19d69
Ines <Ines@MacBook-Pro-de-Ines-2.local>
2019-03-25 18:04:10 -0400

    fix redirect routes

--- client/index.js
+++ client/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import {BrowserRouter as Router} from "react-router-dom";
-import App from './app';
+import { Router } from "react-router-dom";
+import App from "./app";
+import history from "./history";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       <App />
     </Router>
   </Provider>,
