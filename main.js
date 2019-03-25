17c410a4b86850fcc078b125f16f8789e4d1977d
alvinjtang <alvintangdesigns@gmail.com>
2019-03-25 14:51:14 -0400

    updates main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// 'use strict'
+'use strict'
 
-// const db = require('./server/db')
-// const app = require('./server')
-// const PORT = process.env.PORT || 8080
-// const {magenta, green} = require('chalk')
+const db = require('./server/db')
+const app = require('./server')
+const PORT = process.env.PORT || 8080
+const {magenta, green} = require('chalk')
 
-// db.sync()
-//   .then(() => {
-//     console.log(green('db synced'))
-//     app.listen(PORT, () => console.log(magenta(`Listening on port ${PORT}`)))
-//   })
+db.sync()
+  .then(() => {
+    console.log(green('db synced'))
+    app.listen(PORT, () => console.log(magenta(`Listening on port ${PORT}`)))
+  })
